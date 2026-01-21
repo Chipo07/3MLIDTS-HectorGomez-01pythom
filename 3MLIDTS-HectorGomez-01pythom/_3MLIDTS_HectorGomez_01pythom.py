8fe5878d5cc8e9d4b627c05b93a582504b89b4ad
Ishuzoku Reviewers <Ishuzoku Reviewers@DESKTOP-88IR52H>
2026-01-20 21:04:58 -0600

    Agregar archivos de proyecto.

---
+++ 3MLIDTS-HectorGomez-01pythom/_3MLIDTS_HectorGomez_01pythom.py
@@ -0,0 +1,10 @@
+import tkinter as tk 
+
+def main():
+    ventana = tk.Tk()
+    ventana.title("Actividad 01 - Programacion Avanzada")
+    ventana.geometry("400x300")
+    ventana.mainloop()
+
+    if __name__== "__main__":
+        main()
